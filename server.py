b76152cf0c5ca0ebbb30685208e9128af608f4b8
vinzenz-fischer <vhfischer@outlook.de>
2025-01-23 15:12:59 +0100

    primitive matchmaking

--- server.py
+++ server.py
@@ -2,26 +2,56 @@ from threading import Thread
 from socket import socket as Socket, AF_INET, SOCK_STREAM
 from typing import Callable
 
-def default_handler(conn: Socket) -> None:
-    conn.close()
+def default_handler(player1: tuple[Socket, str], player2: tuple[Socket, str]) -> None:
+    player1.close()
+    player2.close()
 
 class Server(Socket):
-    def __init__(self, host: str, port: int, handler: Callable[[Socket, int], None] = default_handler) -> None:
+    def __init__(self,
+            host: str, port: int,
+            match_thread_function: Callable[
+                [tuple[Socket, str], tuple[Socket, str]],
+                None] = default_handler
+        ) -> None:
         super().__init__(AF_INET, SOCK_STREAM)
         self.bind((host, port))
-        self.threads: list[Thread] = []
-        self.handler = handler
+        self.match_thread_target = match_thread_function
+        self.matchmaking_queue: dict[str, Socket] = []
+        self.match_threads: list[Thread] = []
+
+        # self.matched: dict[tuple[Socket, str], int] = {}
     
     def listen(self) -> None:
         self.listen()
 
         while True:
             conn, addr = self.accept()
-            client_idx = len(self.threads)
-            print(f"[SERVER] New connection: {addr} ({client_idx=})")
-            thread = Thread(target=self.handler, args=(conn, addr, client_idx))
-            thread.start() # this server is vulnerable to race conditions
-            self.threads.append(thread)
+            print(f"[SERVER] New connection: {addr}")
+            if addr in self.matchmaking_queue.keys():
+                self.matchmaking_queue.pop(addr)
+                conn.send(r"removed your from queue")
+                print(f"[SERVER] Removed {addr} from queue")
+            else:
+                self.matchmaking_queue[addr] = conn
+                conn.send(r"added to queue")
+                print(f"[SERVER] Added {addr} to queue")
+                self.try_make_match()
+    
+    def try_make_match(self) -> bool:
+        if len(self.matchmaking_queue) < 2:
+            return False
+        
+        # implement match making criteria here
+        # we could ask each player if they wanna accept the match
+        player1 = self.matchmaking_queue.popitem()
+        player2 = self.matchmaking_queue.popitem()
+
+        self.match_threads.append(
+            Thread(target=self.match_thread_target, args=(player1, player2))
+        )
+        self.match_threads[-1].start()
+
+        return True
 
 if __name__ == "__main__":
     server = Server("localhost", 8080)
