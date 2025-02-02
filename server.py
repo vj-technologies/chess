bebfe0b70f5b637b963b71d95f86b626035b5c57
vinzenz-fischer <vhfischer@outlook.de>
2025-02-02 16:24:03 +0100

    incoorp main.py in server to avoid confusion

--- server.py
+++ server.py
@@ -1,22 +1,91 @@
-if __name__ == "__main__":
-    print(f"\"{__file__}\" is supposed to be imported.")
-    exit(1)
-
 from socket import socket as Socket, AF_INET, SOCK_STREAM
 from datetime import datetime
 from threading import Thread
 from typing import Callable
-import os.path
+import os.path, json
+
+from chess_utils import GameState
+from chess_refac import *
+
+def msg_to_move(msg: str) -> dict | None:
+    try:
+        move: dict = json.loads(msg)
+    except Exception:
+        return None
+
+    if not isinstance(move["start_x"], int) or 7 < move["start_x"] or move["start_x"] < 0: return None
+    if not isinstance(move["start_y"], int) or 7 < move["start_y"] or move["start_y"] < 0: return None
+    if not isinstance(move["end_x"  ], int) or 7 < move["end_x"  ] or move["end_x"  ] < 0: return None
+    if not isinstance(move["end_y"  ], int) or 7 < move["end_y"  ] or move["end_y"  ] < 0: return None
+    
+    return move
+
+def chess_match(white: Socket, black: Socket, encoding: str, buffsize: int) -> None:
+    game = Game()
+    black.send("match start".encode(encoding))
+    white.send("match start".encode(encoding))
+    network_error = False
+
+    while game.state == GameState.WHITE_TURN and not network_error:
+        white.send("ur turn".encode(encoding))
+        while game.state == GameState.WHITE_TURN:
+            try:
+                msg = white.recv(buffsize).decode(encoding)
+            except Exception as e:
+                print(f"Network Error occoured: {e}")
+                network_error = True
+                break
+            
+            move = msg_to_move(msg)
+            if game.make_move_if_valid(move, Color.WHITE):
+                # TODO: Send current board configuration or latest move to the other player.
+                break
+            white.send("try again dumbass".encode(encoding))
+
+        if game.state != GameState.BLACK_TURN or network_error:
+            break
+
+        black.send("ur turn".encode(encoding))
+        while game.state == GameState.BLACK_TURN:
+            try:
+                msg = black.recv(buffsize).decode(encoding)
+            except Exception as e:
+                print(f"Network Error occoured: {e}")
+                network_error = True
+                break
+            
+            move = msg_to_move(msg)
+            if game.make_move_if_valid(move, Color.BLACK):
+                # TODO: Send current board configuration or latest move to the other player.
+                break
+            black.send("try again dumbass".encode(encoding))
+    
+    match game.state:
+        case GameState.DRAW:
+            white.send("draw".encode(encoding))
+            black.send("draw".encode(encoding))
+        case GameState.WHITE_WON:
+            white.send("you won".encode(encoding))
+            black.send("you lost".encode(encoding))
+        case GameState.BLACK_WON:
+            white.send("you lost".encode(encoding))
+            black.send("you won".encode(encoding))
+        case _:
+            print(f"Unhandled game ending: {game.state}")
+
+    white.close()
+    black.close()
 
 class Server(Socket):
     def __init__(self, conf: dict, match_thread_function: Callable[[Socket, Socket, str, int], None]) -> None:
         super().__init__(AF_INET, SOCK_STREAM)
         
+        # Explicitly take needed values for readability & autocomlete
         self.addr: tuple[str, int] = (conf["host"], conf["port"])
         self.encoding: str = conf["encoding"]
         self.buffsize: int = conf["buffsize"]
         self.time_stamp_format: str = conf["time_stamp_format"]
-        self.logs_directory: str = conf["logs_directory"]
+        self.logs_dir: str = conf["logs_directory"]
 
         self.bind(self.addr)
         self.match_thread_target: Callable[[Socket, Socket, str, int], None] = match_thread_function
@@ -24,13 +93,13 @@ class Server(Socket):
         self.match_threads: list[Thread] = []
 
         self._log_file = None
-        if os.path.isdir(self.logs_directory):
+        if os.path.isdir(self.logs_dir):
             now = datetime.now()
             filename = now.strftime(conf["filename"])
             self._log_file = f"logs/{filename}.log"
             with open(self._log_file, "w+") as file:
                 file.write(f"[INFO] {now.strftime(self.time_stamp_format)} Log file created.\n")
-        elif conf["logs_directory"] != None:
+        elif self.logs_dir != None:
             print("The provided logs directory couldn't be found. If you don't want log files, just put in `None`.")
             exit(1)
     
@@ -93,3 +162,33 @@ class Server(Socket):
 
         self.info(f"Match created: {player1[0]} vs {player2[0]}")        
         return True
+
+if __name__ == "__main__":
+    # Define the server's default configuration values here:
+    conf = {
+        "host": "127.0.0.1",
+        "port": 9999,
+        "buffsize": 1024,
+        "encoding": "utf-8",
+        "logs_dir": "logs/",
+        "filename": "%Y_%m_%d_%Hh%Mm%Ss",
+        "time_stamp_format": "%d.%m.%Y %H:%M:%S"
+    }
+
+    # load server configuration values
+    server_config_file = "server.conf"
+    if os.path.isfile(server_config_file):
+        with open(server_config_file, "r") as file:
+            for line in file.readlines():
+                line = line.strip()
+                val_key = line.split("=", 1)
+                conf[val_key[0]] = val_key[1]
+    
+    try:
+        conf["port"] = int(conf["port"])
+        conf["buffsize"] = int(conf["buffsize"])
+    except Exception as e:
+        print(e)
+    else:
+        server = Server(conf, chess_match)
+        server.listen()
